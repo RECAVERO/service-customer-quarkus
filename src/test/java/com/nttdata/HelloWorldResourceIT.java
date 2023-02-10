cd35f39333e8afc84a113f31d90aba36a9a47d9b
Reynaldo Cavero Rosales <rcaveror@emeal.nttdata.com>
2023-02-10 17:48:50 -0500

    pruebas unitarias de crud de customer

--- src/test/java/com/nttdata/HelloWorldResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.nttdata;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HelloWorldResourceIT extends HelloWorldResourceTest {
-    // Execute the same tests but in packaged mode.
-}
